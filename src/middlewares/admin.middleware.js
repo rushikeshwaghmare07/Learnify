c011efeaf2542b8c99de64e8d3d2d980ce379a94
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-14 00:57:33 +0530

    update token input

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const adminAuth = async (req, res, next) => {
   try {
-    const token = req.headers.token;
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if (!token) {
       return res.status(401).json({
         success: false,
