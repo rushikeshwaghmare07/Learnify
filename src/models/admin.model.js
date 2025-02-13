e49bb8ae43b219cd0022e48735e868d8a36ea459
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-13 23:34:05 +0530

    feat: add admin schema

---
+++ src/models/admin.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+adminSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const adminModel = mongoose.model("admin", adminSchema);
+
+module.exports = adminModel;
