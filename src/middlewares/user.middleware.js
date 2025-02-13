16a0a41dfee9762c42424fa4b896eccdfdfa0bfe
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-14 00:31:19 +0530

    add user auth middleware

---
+++ src/middlewares/user.middleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authorization failed. Please log in again.",
+      });
+    }
+
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET_USER);
+    if (decodedToken) {
+      req.userId = decodedToken.id;
+      next();
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "Invalid token. Authorization denied.",
+      });
+    }
+  } catch (error) {
+    console.log("Error in user auth middleware: ", error);
+    return res.status(500).json({
+      success: false,
+      message: error?.message || "An unexpected error occurred. Please try again later.",
+    });
+  }
+};
+
+module.exports = userAuth;
